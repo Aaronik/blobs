afa9cfe2da40c1c38230dc5d9b884c6ad9c1f9c4
Aaron Sullivan <aasullivan1618@gmail.com>
2022-10-31 01:52:30 -0700

    Fruitlessly working towards getting trails on the SPS.
    Do have paticles coming from random spots from along the sphere
    emitter's edge (ish)

--- src/visuals/ammo-orb.ts
+++ src/visuals/ammo-orb.ts
@@ -3,12 +3,15 @@ import 'babylonjs-loaders'
 window.CANNON = require('cannon')
 
 // I wish I could get these from the scene object but I haven't figured out how
-// TODO
+// TODO see http://www.babylonjs.com.cn/how_to/solid_particle_system.html section Particle Intersections
 const particleSystems: { [name: string]: BABYLON.SolidParticleSystem } = {}
 
 // Originally nicked from https://playground.babylonjs.com/#1F4UET#33
 
 export const start = async (from: BABYLON.Mesh, to: BABYLON.Mesh, scene: BABYLON.Scene) => {
+
+  // const numParticles = from.getBoundingInfo().boundingSphere.radius
+  const numParticles = 10
 
   // // Set up new rendering pipeline for glow // TODO This is an important visual effect
   // const pipeline = new BABYLON.DefaultRenderingPipeline("default", true, scene, [camera])
@@ -25,12 +28,16 @@ export const start = async (from: BABYLON.Mesh, to: BABYLON.Mesh, scene: BABYLON
   // scene.enablePhysics(to.position.subtract(from.position))
 
   /************* THESE ARE THE GOODS *******************/
-  // const pinkEnergyBall = await BABYLON.SceneLoader.LoadAssetContainerAsync("pinkEnergyBall.glb", undefined, scene)
-  // const orb = pinkEnergyBall.meshes[0]
+
   // const orbParentSphere = BABYLON.MeshBuilder.CreateSphere("s", { segments: 16, diameter: 0.7 }, scene)
   // orbParentSphere.isVisible = false
+
+  // TODO This goes to initParticles
   // orb.setParent(orbParentSphere)
-  // orbParentSphere.position = from.position
+
+  // orbParentSphere.bakeCurrentTransformIntoVertices()
+  // orbParentSphere.computeWorldMatrix(true)
+
   // orbParentSphere.scaling.scaleInPlace(0.3) // TODO What's this for
   // orb.scaling.scaleInPlace(0.03) // TODO What's this for
   // orb.rotation.set(Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI) // TODO What's this for
@@ -41,61 +48,59 @@ export const start = async (from: BABYLON.Mesh, to: BABYLON.Mesh, scene: BABYLON
   //   restitution: 0.6
   // })
 
-  // const trail = new BABYLON.TrailMesh('orb trail', orb, scene, 0.2, 30, true)
-
-  // const sourceMat = new BABYLON.StandardMaterial('sourceMat', scene) // This'll be the material on the trail
-
-  // const color = BABYLON.Color3.Red()
-
-  // sourceMat.emissiveColor = sourceMat.diffuseColor = color
-  // sourceMat.specularColor = BABYLON.Color3.Black()
-
-  // trail.material = sourceMat
   /**************** THE GOODS ***************************/
 
-  // // TODO Doubt we need this here
-  // scene.addMesh(orb, true)
+  // // TODO COnsider whether this is necessary
+  // scene.addMesh(orbParentSphere)
 
-  const SPS = new BABYLON.SolidParticleSystem("SPS-" + from.name, scene)
+  const SPS = new BABYLON.SolidParticleSystem("SPS-" + from.name, scene, { useModelMaterial: true })
   particleSystems[from.name] = SPS
   SPS.computeParticleRotation = false
 
-  const orbParentSphere = BABYLON.MeshBuilder.CreateSphere('particle-sphere', { segments: 16, diameter: 1 }, scene)
+  const orbParentSphere = BABYLON.MeshBuilder.CreateSphere('sphere', { segments: 16, diameter: 1 }, scene)
+  SPS.addShape(orbParentSphere, numParticles)
+  orbParentSphere.dispose()
 
-  // If I use orb as BABYLON.Mesh here it'll jump out fast, but then i get an error, and it still doesn't look right.
-  SPS.addShape(orbParentSphere, 20)
+  const trail = new BABYLON.TrailMesh('trail', orbParentSphere, scene, 0.2, 30, true)
+  const sourceMat = new BABYLON.StandardMaterial('sourceMat', scene) // This'll be the material on the trail
+  const color = BABYLON.Color3.Red()
+  sourceMat.emissiveColor = sourceMat.diffuseColor = color
+  sourceMat.specularColor = BABYLON.Color3.Black()
+  trail.material = sourceMat
+  SPS.addShape(trail, numParticles)
+  trail.dispose()
 
-  orbParentSphere.dispose() // free memory
+  const pinkEnergyBall = await BABYLON.SceneLoader.LoadAssetContainerAsync("pinkEnergyBall.glb", undefined, scene)
+  const orb = pinkEnergyBall.meshes[0]
+  orb.setParent(orbParentSphere)
+  // SPS.addShape(orb as BABYLON.Mesh, numParticles) // TODO This casting may cause issues
 
-  const mesh = SPS.buildMesh() // finally builds and displays the SPS mesh
+  SPS.buildMesh() // finally builds and displays the SPS mesh
+
+  const getRandomBoundingPosition = (mesh: BABYLON.Mesh) => {
+    const emitterRadius = from.getBoundingInfo().boundingSphere.radiusWorld
+    const vec = new BABYLON.Vector3(
+      1 - (Math.random() * 2),
+      1 - (Math.random() * 2),
+      1 - (Math.random() * 2)
+    ).normalize().scale(emitterRadius)
+    return mesh.position.add(vec)
+  }
 
   const recycleParticle = (particle: BABYLON.SolidParticle) => {
-    particle.position = from.position.clone()
+    particle.position = getRandomBoundingPosition(from)
+    particle.velocity = to.position.subtract(particle.position).normalize().scale(Math.random() * 0.1)
     return particle
   }
 
   SPS.initParticles = () => {
     for (let p = 0; p < SPS.nbParticles; p++) {
       const particle = SPS.particles[p]
-      //Place particles at random positions with a cube
-      // particle.position.x = BABYLON.Scalar.RandomRange(-50, 50)
-      // particle.position.y = BABYLON.Scalar.RandomRange(-50, 50)
-      // particle.position.z = BABYLON.Scalar.RandomRange(-50, 50)
-
-      particle.position = from.position.clone()
-      particle.velocity = to.position.subtract(from.position).normalize().scale(Math.random())
+      recycleParticle(particle)
     }
   }
 
   SPS.updateParticle = (particle) => {
-    // let hasCollidedWithDestination: boolean = false
-    // const boundingInfo = particle.getBoundingInfo()
-
-    // if (!!boundingInfo) { // The bounding info never seems to come from these
-    //   alert('got that bounding info')
-    //   hasCollidedWithDestination = particle.position.equals(to.position)
-    // }
-
     const distanceTraveled = particle.position.subtract(from.position).length()
     const distanceToTravel = to.position.subtract(from.position).length()
 
@@ -122,30 +127,6 @@ export const start = async (from: BABYLON.Mesh, to: BABYLON.Mesh, scene: BABYLON
     SPS.setParticles()
   })
 
-  // let orbs = [pinkEnergyBall.meshes[0]]
-
-  // orbs = orbs.map(orb => {
-  //   // Create lights
-  //   // const pointLight = new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 3, 0), scene)
-  //   // pointLight.intensity = 0.3
-
-  //   // Add physics root for spheres
-  //   const sphereOpts = {
-  //     segments: 16,
-  //     diameter: 0.7
-  //   }
-
-  //   const sphere = BABYLON.MeshBuilder.CreateSphere("s", sphereOpts, scene)
-  //   sphere.isVisible = false
-  //   orb.setParent(sphere)
-  //   sphere.position = from.position
-  //   sphere.scaling.scaleInPlace(0.3)
-  //   orb.scaling.scaleInPlace(0.03)
-  //   orb.rotation.set(Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI)
-  //   orb.scaling.z *= Math.random() > 0.5 ? -1 : 1
-  //   scene.addMesh(orb, true)
-  //   return sphere
-  // })
 
   // // Create physics impostors // This ground looked great in the example
   // const ground = BABYLON.Mesh.CreateBox("Ground", 1, scene)
@@ -157,26 +138,6 @@ export const start = async (from: BABYLON.Mesh, to: BABYLON.Mesh, scene: BABYLON
   // ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {
   //   mass: 0,
   //   restitution: 0.6
-  // })
-
-  // Add trail to orbs
-  // orbs.forEach((orb, i) => {
-  //   orb.physicsImpostor = new BABYLON.PhysicsImpostor(orb, BABYLON.PhysicsImpostor.SphereImpostor, {
-  //     mass: 1,
-  //     restitution: 0.6
-  //   })
-  //   const trail = new BABYLON.TrailMesh('orb trail', orb, scene, 0.2, 30, true)
-  //   const sourceMat = new BABYLON.StandardMaterial('sourceMat', scene)
-  //   let color = BABYLON.Color3.Red()
-  //   if (i == 1) {
-  //     color = BABYLON.Color3.Green()
-  //   } else if (i == 2) {
-  //     color = BABYLON.Color3.Yellow()
-  //   }
-  //   sourceMat.emissiveColor = sourceMat.diffuseColor = color
-  //   sourceMat.specularColor = BABYLON.Color3.Black()
-
-  //   trail.material = sourceMat
   // })
 }
 
